feb65830f5ead3f9c04b86788505c1ad2e607b2b
Raphael Costa <raphael@localhost.localdomain>
2021-02-01 21:45:51 -0400

    Initial commit

---
+++ lib/review_scraper.ex
@@ -0,0 +1,18 @@
+defmodule ReviewScraper do
+  @moduledoc """
+  Documentation for `ReviewScraper`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ReviewScraper.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/review_scraper_test.exs
@@ -0,0 +1,8 @@
+defmodule ReviewScraperTest do
+  use ExUnit.Case
+  doctest ReviewScraper
+
+  test "greets the world" do
+    assert ReviewScraper.hello() == :world
+  end
+end
