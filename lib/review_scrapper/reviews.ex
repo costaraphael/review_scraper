a99dc324e2edb20b23537549d2e8b4a2217f1ade
Raphael Costa <vidal.raphael@gmail.com>
2021-02-02 21:24:41 -0400

    Make the reviews fetching happen in parallel

--- lib/review_scrapper/reviews.ex
+++ lib/review_scrapper/reviews.ex
@@ -26,7 +26,7 @@ defmodule ReviewScraper.Reviews do
     page_count = Keyword.get(opts, :pages, 5)
     http_options = Keyword.get(opts, :http_options, [])
 
-    with {:ok, pages} <- get_pages(page_count, http_options, []),
+    with {:ok, pages} <- get_pages(page_count, http_options),
          do: {:ok, List.flatten(pages)}
   end
 
@@ -52,12 +52,21 @@ defmodule ReviewScraper.Reviews do
     end
   end
 
-  defp get_pages(0, _http_options, pages), do: {:ok, pages}
-
-  defp get_pages(page_number, http_options, pages) do
-    with {:ok, body} <- fetch_page(page_number, http_options),
-         {:ok, page} <- parse_reviews(page_number, body),
-         do: get_pages(page_number - 1, http_options, [page | pages])
+  defp get_pages(page_count, http_options) do
+    1..page_count
+    |> Task.async_stream(
+      fn page_number ->
+        with {:ok, body} <- fetch_page(page_number, http_options),
+             {:ok, page} <- parse_reviews(page_number, body),
+             do: {:ok, page}
+      end,
+      timeout: 10_000
+    )
+    |> Enum.map(fn {:ok, result} -> result end)
+    |> Enum.reduce_while({:ok, []}, fn
+      {:ok, page}, {:ok, pages} -> {:cont, {:ok, [page | pages]}}
+      {:error, error}, _acc -> {:halt, {:error, error}}
+    end)
   end
 
   defp fetch_page(page_number, http_options) do
